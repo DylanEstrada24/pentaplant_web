20ceb3d0366fefbbf87c6571f3d368b3c2b58155
kimkevin2657@naver.com <kimkevin2657>
2021-09-06 20:35:51 +0900

    footer change kev

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -6,9 +6,9 @@ function Footer() {
         <footer>
             <div>
                 <div class="footer_comp_info">
-                    주식회사 펜타플렌트 | 대표 : 홍길동 <br/>
+                    주식회사 펜타플렌트            <br/>
                     사업자 등록번호 : 000-00-0000 <br/>
-                    부산광역시 남구 전포대로 00 <br/>
+                    부산광역시 해운대구 센텀1로 28 <br/>
                     support@pentaplant.com
                 </div>
                 <div class="footer_menu_container">
